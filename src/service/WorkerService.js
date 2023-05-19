cadea07d17e459b77d50a4ca764ae14d378ea216
ozturkmelihcan <melihcannozturkk@gmail.com>
2023-05-19 18:14:56 +0300

    forgot page , worker profile page

--- src/service/WorkerService.js
+++ src/service/WorkerService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const GET_INFO_FOR_WORKER = "http://localhost:9093/api/v1/workers/worker/";
 
 
-
+//silersin
 class WorkerService {
   getInfoForWorker(token) {
     return axios.get(GET_INFO_FOR_WORKER + token);
